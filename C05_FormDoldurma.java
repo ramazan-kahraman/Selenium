89bbe2580217abd6425df6b9a7055e7df990cd95
rkah1381 <119287759+rkah1381@users.noreply.github.com>
2023-01-11 10:31:50 +0300

    Add files via upload

---
+++ C05_FormDoldurma.java
@@ -0,0 +1,72 @@
+package day09_actions;
+
+import com.github.javafaker.Faker;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import utulities.ReuseableMethods;
+import utulities.TestBase;
+
+import javax.swing.*;
+
+public class C05_FormDoldurma extends TestBase {
+
+    @Test
+    public void test01(){
+
+        // facebook.com sayfasina gidin
+
+        driver.get("https://www.facebook.com");
+
+        // yeni hesap olustur butonuna basin
+
+        driver.findElement(By.xpath("//a[@class='_42ft _4jy0 _6lti _4jy6 _4jy2 selected _51sy']")).click();
+
+        // ilgili olanlari faker kutuphanesinden degerlerle doldurup
+
+        WebElement firstnameKutusu=driver.findElement(By.name("firstname"));
+
+        Actions actions=new Actions(driver);
+        Faker faker=new Faker();
+
+        String mailAdresi=faker.internet().emailAddress();
+
+        actions.click(firstnameKutusu)
+                .sendKeys(faker.name().firstName())
+                .sendKeys(Keys.TAB)
+                .sendKeys(faker.name().lastName())
+                .sendKeys(Keys.TAB)
+                .sendKeys(mailAdresi)
+                .sendKeys(Keys.TAB)
+                .sendKeys(mailAdresi)
+                .sendKeys(Keys.TAB)
+                .sendKeys(faker.internet().password())
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys("23")
+                .sendKeys(Keys.TAB)
+                .sendKeys("Oct")
+                .sendKeys(Keys.TAB)
+                .sendKeys("1990")
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.ARROW_RIGHT)
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB).perform();
+
+        // kaydol butonuna basin
+
+        WebElement signupButonu=driver.findElement(By.name("websubmit"));
+        signupButonu.click();
+
+
+
+        ReuseableMethods.bekle(20);
+    }
+}
